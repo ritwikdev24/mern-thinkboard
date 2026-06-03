acfae3d60a59a98734a18a79cf3d1b9442ca676a
rithvik <rithvikkoppisetti@gmail.com>
2026-06-03 15:41:34 +0530

    Fix CORS for Vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,10 @@ const PORT = process.env.PORT || 5001;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://mern-thinkboard-delta.vercel.app",
+    ],
   })
 );
 
